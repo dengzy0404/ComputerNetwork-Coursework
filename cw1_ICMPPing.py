cdb0db3f390932a8f4682e8521468948725f491c
dengzy <2762206548@qq.com>
2023-11-15 19:16:44 +0800

    with 6 features

--- cw1_ICMPPing.py
+++ cw1_ICMPPing.py
@@ -1,40 +1,38 @@
 import socket
 import os
-import sys
 import struct
 import time
 import select
-import binascii  
 
 
-ICMP_ECHO_REQUEST = 8 #ICMP type code for echo request messages
-ICMP_ECHO_REPLY = 0 #ICMP type code for echo reply messages
+ICMP_ECHO_REQUEST = 8  # ICMP type code for echo request messages
 
 
-def checksum(string): 
+def checksum(string):
     csum = 0
-    countTo = (len(string) // 2) * 2  
+    countTo = (len(string) // 2) * 2
     count = 0
 
     while count < countTo:
-        thisVal = string[count+1] * 256 + string[count]
-        csum = csum + thisVal 
-        csum = csum & 0xffffffff  
+        thisVal = string[count + 1] * 256 + string[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
         count = count + 2
-    
+
     if countTo < len(string):
         csum = csum + string[len(string) - 1]
-        csum = csum & 0xffffffff 
-    
+        csum = csum & 0xffffffff
+
     csum = (csum >> 16) + (csum & 0xffff)
     csum = csum + (csum >> 16)
-    answer = ~csum 
-    answer = answer & 0xffff 
+    answer = ~csum
+    answer = answer & 0xffff
     answer = answer >> 8 | (answer << 8 & 0xff00)
 
     answer = socket.htons(answer)
 
     return answer
+
 
 def receiveOnePing(icmpSocket, ID, timeout):
     timeLeft = timeout
@@ -43,7 +41,7 @@ def receiveOnePing(icmpSocket, ID, timeout):
         whatReady = select.select([icmpSocket], [], [], timeLeft)
         howLongInSelect = (time.time() - startedSelect)
         if whatReady[0] == []:  # Timeout
-            return "Request timed out.", None
+            return "Request timed out.", None, None
 
         timeReceived = time.time()
         recPacket, addr = icmpSocket.recvfrom(1024)
@@ -57,11 +55,11 @@ def receiveOnePing(icmpSocket, ID, timeout):
         if packetID == ID:
             bytesInDouble = struct.calcsize("d")
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
-            return timeReceived - timeSent, ttl
+            return timeReceived - timeSent, ttl, code
 
         timeLeft = timeLeft - howLongInSelect
         if timeLeft <= 0:
-            return "Request timed out.", None
+            return "Request timed out.", None, None
 
 
 def sendOnePing(icmpSocket, destAddr, ID):
@@ -79,13 +77,13 @@ def doOnePing(destAddr, timeout):
     icmp = socket.getprotobyname("icmp")
     icmpSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
 
-    myID = os.getpid() & 0xFFFF  # Return the current process i
+    myID = os.getpid() & 0xFFFF  # Return the current process ID
 
     sendOnePing(icmpSocket, destAddr, myID)
-    delay, ttl = receiveOnePing(icmpSocket, myID, timeout)
+    delay, ttl, code = receiveOnePing(icmpSocket, myID, timeout)
 
     icmpSocket.close()
-    return delay, ttl
+    return delay, ttl, code
 
 
 def ping(host, timeout=1, count=4):
@@ -95,27 +93,42 @@ def ping(host, timeout=1, count=4):
     print(f"Pinging {host} [{destAddr}] with {packetSize} bytes of data:")
 
     responses = []
+    total_delay = 0
+    min_delay = float('inf')
+    max_delay = 0
+    packet_loss_count = 0
 
     for _ in range(count):
-        delay, ttl = doOnePing(destAddr, timeout)
+        delay, ttl, code = doOnePing(destAddr, timeout)
 
-        if isinstance(delay, float):
-            delay_ms = delay * 1000
-            responses.append(delay_ms)
-            print(f"Reply from {destAddr}: bytes={packetSize} RRT={delay_ms:.2f}ms TTL={ttl}")
-        else:
-            print(f"Reply from {destAddr}: {delay}")
+        if code is not None:
+            if code == 0:
+                if isinstance(delay, float):
+                    delay_ms = delay * 1000
+                    total_delay += delay_ms
+                    responses.append(delay_ms)
+                    min_delay = min(min_delay, delay_ms)
+                    max_delay = max(max_delay, delay_ms)
+                    print(f"Reply from {destAddr}: bytes={packetSize} RRT={delay_ms:.2f}ms TTL={ttl}")
+                else:
+                    print(f"Reply from {destAddr}: {delay}")
+            else:
+                packet_loss_count += 1
+                print(f"ICMP Error: {code}")
 
         time.sleep(1)
 
     # Printing the statistics
     print("\n{} packets transmitted, {} received, {}% packet loss".format(count, len(responses),
-                                                                           ((count - len(responses)) / count) * 100))
+                                                                           (packet_loss_count / count) * 100))
 
     if responses:
-        print("Round-trip min/avg/max = {:.2f}/{:.2f}/{:.2f} ms".format(min(responses), sum(responses) / len(responses),
-                                                                        max(responses)))
+        avg_delay = total_delay / len(responses)
+        print("Round-trip min/avg/max = {:.2f}/{:.2f}/{:.2f} ms".format(min_delay, avg_delay, max_delay))
 
 
 if __name__ == '__main__':
-    ping("lancaster.ac.uk")
+    host = input("Enter the IP address or hostname to ping: ")
+    timeout = float(input("Enter the timeout value (in seconds): "))
+    count = int(input("Enter the number of packets to send: "))
+    ping(host, timeout, count)
